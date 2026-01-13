28e17c28bbdf31bed5d12e7015ef73b87d684cdf
iifar sahdan <namagwpia@gmail.com>
2026-01-13 15:52:40 +0700

    telegram anonymous chat bot

---
+++ app/config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+DB_CONFIG = {
+    "host": os.getenv("DB_HOST", "localhost"),
+    "user": os.getenv("DB_USER", "root"),
+    "password": os.getenv("DB_PASSWORD", ""),
+    "database": os.getenv("DB_NAME", "anonymous_chat"),
+}
